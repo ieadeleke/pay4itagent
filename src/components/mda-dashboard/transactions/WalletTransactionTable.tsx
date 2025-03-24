8afd1bd566da43fbcc63dbd31d7f0923de6c947c
ieadeleke <eadelekeife@yahoo.com>
2025-03-24 10:42:02 +0000

    index route

--- src/components/mda-dashboard/transactions/WalletTransactionTable.tsx
+++ src/components/mda-dashboard/transactions/WalletTransactionTable.tsx
@@ -201,28 +201,52 @@ export const TransactionTable = (props: TransactionTableProps) => {
             let typeData = {
                 key: "category",
                 label: "Category",
-                children: props.categoryEnum.map((type: string, index: number) => ({
-                    key: `${index}-${index}`,
-                    label: <Button
-                        variant="text"
-                        className="px-6 py-0"
-                        onClick={() => {
-                            type = type === "ALL" ? "" : type;
-                            fetchTransactions({
-                                walletId: props.walletId ? props.walletId : "",
-                                startDate: convertDateToSecondFormat(date.from),
-                                endDate: convertDateToSecondFormat(date.to ?? new Date()),
-                                type: selectedType,
-                                category: type
-                            })
-                            setSelectedCategory(type);
-                            selectedCategory = type;
-                            setFilterEnabled(true);
-                        }}
-                    >
-                        {type.split("_").join(" ")}
-                    </Button>
-                }))
+                children: [
+                    {
+                        key: "type-all",
+                        label: <Button
+                            variant="text"
+                            className="px-6 py-0"
+                            onClick={() => {
+                                fetchTransactions({
+                                    walletId: props.walletId ? props.walletId : "",
+                                    startDate: convertDateToSecondFormat(date.from),
+                                    endDate: convertDateToSecondFormat(date.to ?? new Date()),
+                                    type: selectedType,
+                                    category: ""
+                                })
+                                setSelectedCategory("");
+                                selectedCategory = "";
+                                setFilterEnabled(true);
+                            }}
+                        >
+                            All
+                        </Button>
+                    }
+                    , {
+                        ...props.categoryEnum.map((type: string, index: number) => ({
+                            key: `${index}-${index}`,
+                            label: <Button
+                                variant="text"
+                                className="px-6 py-0"
+                                onClick={() => {
+                                    type = type === "ALL" ? "" : type;
+                                    fetchTransactions({
+                                        walletId: props.walletId ? props.walletId : "",
+                                        startDate: convertDateToSecondFormat(date.from),
+                                        endDate: convertDateToSecondFormat(date.to ?? new Date()),
+                                        type: selectedType,
+                                        category: type
+                                    })
+                                    setSelectedCategory(type);
+                                    selectedCategory = type;
+                                    setFilterEnabled(true);
+                                }}
+                            >
+                                {type.split("_").join(" ")}
+                            </Button>
+                        }))
+                    }]
             }
             setCategoryEnum(typeData);
         }
